769e7ca62deb30fe141c02da49aad13ce2b9fc1e
apple <you@example.com>
2016-08-18 12:19:54 +0800

    修改了背景颜色

--- herolife/herolife/ViewController.m
+++ herolife/herolife/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+    
+    self.view.backgroundColor=[UIColor yellowColor];
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
