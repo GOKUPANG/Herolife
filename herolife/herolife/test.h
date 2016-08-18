1ccc5c6b0b1552db39f0755a6df9152da177020b
null <null>
2016-08-18 12:23:49 +0800

    添加test文件

---
+++ herolife/herolife/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  herolife
+//
+//  Created by sswukang on 16/8/18.
+//  Copyright © 2016年 huarui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
