7661c0ad95f24170530c7c5c89d50deaac25a8df
null <null>
2016-08-19 17:11:42 +0800

    修复登陆界面的一些bug

---
+++ herolife/herolife/Classes/Other-其他/Category/UIView+HRExtension.h
@@ -0,0 +1,20 @@
+//
+//  UIView+HRExtension.h
+//  xiaorui
+//
+//  Created by sswukang on 16/4/6.
+//  Copyright © 2016年 huarui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (HRExtension)
+@property (nonatomic, assign) CGSize hr_size;
+@property (nonatomic, assign) CGFloat hr_width;
+@property (nonatomic, assign) CGFloat hr_height;
+@property (nonatomic, assign) CGFloat hr_x;
+@property (nonatomic, assign) CGFloat hr_y;
+@property (nonatomic, assign) CGFloat hr_centerX;
+@property (nonatomic, assign) CGFloat hr_centerY;
+
+@end
---
+++ herolife/herolife/Classes/Other-其他/Category/UIView+HRExtension.m
@@ -0,0 +1,98 @@
+//
+//  UIView+HRExtension.m
+//  xiaorui
+//
+//  Created by sswukang on 16/4/6.
+//  Copyright © 2016年 huarui. All rights reserved.
+//
+
+#import "UIView+HRExtension.h"
+
+@implementation UIView (HRExtension)
+
+- (CGSize)hr_size
+{
+	return self.frame.size;
+}
+
+- (void)setHr_size:(CGSize)hr_size
+{
+	CGRect frame = self.frame;
+	frame.size = hr_size;
+	self.frame = frame;
+}
+
+- (CGFloat)hr_width
+{
+	return self.frame.size.width;
+}
+
+- (CGFloat)hr_height
+{
+	return self.frame.size.height;
+}
+
+- (void)setHr_width:(CGFloat)hr_width
+{
+	CGRect frame = self.frame;
+	frame.size.width = hr_width;
+	self.frame = frame;
+}
+
+- (void)setHr_height:(CGFloat)hr_height
+{
+	CGRect frame = self.frame;
+	frame.size.height = hr_height;
+	self.frame = frame;
+}
+
+- (CGFloat)hr_x
+{
+	return self.frame.origin.x;
+}
+
+- (void)setHr_x:(CGFloat)hr_x
+{
+	CGRect frame = self.frame;
+	frame.origin.x = hr_x;
+	self.frame = frame;
+}
+
+- (CGFloat)hr_y
+{
+	return self.frame.origin.y;
+}
+
+- (void)setHr_y:(CGFloat)hr_y
+{
+	CGRect frame = self.frame;
+	frame.origin.y = hr_y;
+	self.frame = frame;
+}
+
+- (CGFloat)hr_centerX
+{
+	return self.center.x;
+}
+
+- (void)setHr_centerX:(CGFloat)hr_centerX
+{
+	CGPoint center = self.center;
+	center.x = hr_centerX;
+	self.center = center;
+}
+
+
+- (CGFloat)hr_centerY
+{
+	return self.center.y;
+}
+
+- (void)setHr_centerY:(CGFloat)hr_centerY
+{
+	CGPoint center = self.center;
+	center.y = hr_centerY;
+	self.center = center;
+}
+
+@end
